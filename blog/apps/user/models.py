fd77ceda510b07a3f25615d10934365d3e347ea8
german98 <germandelfino22@gmail.com>
2025-07-24 19:16:10 -0300

    Agregar modelo de usuario personalizado con campo de avatar y alias

--- blog/apps/user/models.py
+++ blog/apps/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+import os
 
-# Create your models here.
+
+def get_avatar_filename(instance, filename):
+    # avatar_default.jpg
+
+    base_filename, file_extension = os.path.splitext(filename)
+    new_filename = f"user_{instance.id}_avatar{file_extension}"
+    return os.path.join('user/avatar/', new_filename)
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    alias = models.CharField(max_length=50, blank=True)
+    avatar = models.ImageField(upload_to=get_avatar_filename, default='user/default/avatar_default.png')
+    
+    def __str__(self):
+        return self.username
+    
+    
